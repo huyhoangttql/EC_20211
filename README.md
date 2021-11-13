003e010142a5996dd4d2d04aefbf05fee483da8e
huyhoangttql <huyhoangttql@gmail.com>
2021-11-13 23:03:32 +0700

    "user can load file input to create obstacles"

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EC_20211
+* Create map with static and dynamic obstacles for path finding problems.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 from DynamicObs import *
 from StaticObs import *
 pygame.init()
@@ -19,26 +20,23 @@ x_robot = y_robot = 100
 x_goal = 700
 y_goal = 500
 
-ls = [[200,300,50,70],
-    [225,270,60,30],
-    [400,400,30,70],
-    [550,150,70,25],
-    [590,175,30,45]]
-
 staticObss = []
-for i in ls:
-    staticObs = StaticObs(screen,i[0],i[1],i[2],i[3])
-    staticObss.append(staticObs)
+dynamicObss = []
+with open ("input1.json") as f:
+    data = json.load(f)
+    for i in data:
+        if i["Dynamic"] == "0":
+            staticObs = StaticObs(screen,int(i["startpointX"]),int(i["startpointY"]),int(i["length"]),int(i["width"]))
+            staticObss.append(staticObs)
+        if i["Dynamic"] == "1":
+            dynamicObs = DynamicObs(screen,int(i["dynX"]),int(i["dynY"]),int(i["speedX"]),
+            int(i["speedY"]),int(i["lower_limit"]),int(i["upper_limit"]),int(i["radius"]))
+            dynamicObss.append(dynamicObs) 
+
 
 
 clock = pygame.time.Clock()
-dynX1=150
-dynY1=250
-speedX1 = 4
-speedY1 = 3
-radius = 10
-dynObs1 = DynamicObs(screen,dynX1,dynY1,speedX1,speedY1,150,300,radius)
-dynObs2 = DynamicObs(screen,300,350,-4,5,250,400,10)
+
 while running:
     screen.fill(white)
     for event in pygame.event.get():
@@ -48,10 +46,11 @@ while running:
 
     pygame.draw.circle(screen,yellow,(x_robot,y_robot),12)
     pygame.draw.rect(screen,red,[x_goal,y_goal,20,20])
-    for j in staticObss:
-        j.draw()
-    dynObs1.move()
-    dynObs2.move()
+    for obstacle in staticObss:
+        # print(j)
+        obstacle.draw()
+    for obstacle in dynamicObss:
+        obstacle.move()
 
 
     pygame.display.update()
